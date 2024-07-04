1f2ce46f08dfb35ecf9406a3e47f106767630023
RexshackGaming <rexshackuk@gmail.com>
2024-07-04 14:54:17 +0100

    readme update

--- installation/shared_items.lua
+++
@@ -1,3 +0,0 @@
-
-    -- guarma boat travel
-    ['boatticket'] = {['name'] = 'boatticket', ['label'] = 'Boat Ticket', ['weight'] = 0, ['type'] = 'item', ['image'] = 'boatticket.png', ['unique'] = false, ['useable'] = false, ['shouldClose'] = true, ['combinable'] = nil, ['level'] = 0, ['description'] = 'used for boat travel'},
